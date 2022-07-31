4d18829b813103bc9850d9323afd59943243df2a
V. M. Shahbaaz Khan <khanvms9@gmail.com>
2022-07-31 21:51:08 +0530

    added pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,40 @@
 import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Login from "./components/Login/Login";
+import RegisterUser from "./components/Register/RegisterUser";
+import RegisterAdmin from "./components/Register/RegisterAdmin";
+import Header from "./components/Header/header";
 
-function App() {
-  const [data,setData]=React.useState(null);
-  React.useEffect(() => {
-    fetch("./api")
-    .then((res) => res.json())
-    .then((data) => setData(data.message));
-  },[]);
-  
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {!data? "Loading...":data}
-        </p>
-      </header>
-    </div>
-  );
-}
+
+//   const [data,setData]=React.useState(null);
+//   React.useEffect(() => {
+//     fetch("./api")
+//     .then((res) => res.json())
+//     .then((data) => setData(data.message));
+//   },[]);
+
+// <BrowserRouter>
+//     <Header/>
+//     <Routes>
+//         <Route path="/" component={Login} exact />
+//         <Route path="/login" component={() => <Login/>} />
+//         <Route path="/registerUser" component={RegisterUser} exact/>
+//         <Route path="/registerAdmin" component={RegisterAdmin} exact/> 
+//     </Routes>
+    
+// </BrowserRouter>
+
+
+const App = () => (
+
+    <Login/>
+)
+
+// function App(){
+//   return(
+//   );
+// }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 
 const path=require('path');
 const express=require("express");
-
+const userRoutes=require('./routes/userRoutes');
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
 const PORT = process.env.PORT || 3001;
+const dotenv=require("dotenv");
+//const connectDB=require("./config/db");
 
 const app=express();
+dotenv.config();
+//connectDB();
+app.use(express.json());
+
+
 
 app.use(express.static(path.resolve(__dirname,'../client/build')));
 
@@ -12,10 +20,14 @@ app.get("/api",(req,res) =>{
     res.json({ message: "Hello from server!"});
 });
 
+app.use('/api/users',userRoutes)
+
+app.use(notFound);
+app.use(errorHandler);
 //all other get requess not handled before will return our react app 
-app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'../client/build','index.html'));
-});
+// app.get('*',(req,res)=>{
+//     res.sendFile(path.resolve(__dirname,'../client/build','index.html'));
+// });
 
 app.listen(PORT,() =>{
     console.log(`Server listening on ${PORT}`);
