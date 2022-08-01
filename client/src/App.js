c5ad43a4520a45d44356206f813c02c25959d140
V. M. Shahbaaz Khan <khanvms9@gmail.com>
2022-08-01 18:46:49 +0530

    repo files created back

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Login from "./components/Login/Login";
+import RegisterUser from "./components/Register/RegisterUser";
+import RegisterAdmin from "./components/Register/RegisterAdmin";
+import Header from "./components/Header/header";
+
+
+
+
+// <BrowserRouter>
+//     <Header/>
+//     <Routes>
+//         <Route path="/" component={Login} exact />
+//         <Route path="/login" component={() => <Login/>} />
+//         <Route path="/registerUser" component={RegisterUser} exact/>
+//         <Route path="/registerAdmin" component={RegisterAdmin} exact/> 
+//     </Routes>
+    
+// </BrowserRouter>
+
+
+const App = () => {
+    const [data,setData]=React.useState(null);
+      React.useEffect(() => {
+        fetch("./api")
+        .then((res) => res.json())
+        .then((data) => setData(data.message));
+      },[]);
+      console.log(data);
+  return( <Login/>);
+}
+
+// function App(){
+//   return(
+//   );
+// }
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,37 @@
+
+const path=require('path');
+const express=require("express");
+const userRoutes=require('./routes/userRoutes');
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
+const PORT = process.env.PORT || 3001;
+const dotenv=require("dotenv");
+const connectDB=require("./config/db");
+
+const app=express();
+dotenv.config();
+connectDB();
+app.use(express.json());
+
+
+
+app.use(express.static(path.resolve(__dirname,'../client/build')));
+
+app.get("/api",(req,res) =>{
+    res.json({ message: "Hello from server!"});
+});
+app.get('*', function(req, res) {
+    res.sendFile('index.html', {root: path.join(__dirname, '../client/build/')});
+  });
+
+app.use('/api/users',userRoutes)
+
+app.use(notFound);
+app.use(errorHandler);
+//all other get requess not handled before will return our react app 
+// app.get('*',(req,res)=>{
+//     res.sendFile(path.resolve(__dirname,'../client/build','index.html'));
+// });
+
+app.listen(PORT,() =>{
+    console.log(`Server listening on ${PORT}`);
+});
