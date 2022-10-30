bd94f7985fd5682ab6e7889f4bedeb5f6e570b19
V. M. Shahbaaz Khan <khanvms9@gmail.com>
2022-10-30 18:39:49 +0530

    database_URI added

--- server/config/mongodbConnect.js
+++ server/config/mongodbConnect.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
-const env=require("../environment/environment")
+//const env=require("../environment/environment")
 
 mongoose.Promise=global.Promise;
 
-const mongoUri=`mongodb://${env.dbName}:${env.key}@${env.dbName}.mongo.cosmos.azure.com:${env.port}/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@${env.dbName}@`
-
+//const mongoUri=`mongodb://${env.dbName}:${env.key}@${env.dbName}.mongo.cosmos.azure.com:${env.port}/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@${env.dbName}@`
+if(!process.env.DATABASE_URL){
+    throw new Error("no value in DATABASE_URL in env var");
+}
+const mongoUri=process.env.DATABASE_URL;
 function connect(){
     return mongoose.connect(mongoUri);
 }
